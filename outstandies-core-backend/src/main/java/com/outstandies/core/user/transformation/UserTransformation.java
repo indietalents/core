ddcaa4d6f16f6d5197a214d2c7fdf4c1b5cdb8a8
indie talents admin <indietalents@gmail.com>
2018-01-26 00:27:59 +0300

    remove unuseful

--- outstandies-core-backend/src/main/java/com/outstandies/core/user/transformation/UserTransformation.java
+++
@@ -1,9 +0,0 @@
-package com.outstandies.core.user.transformation;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.outstandies.core.user.model.User;
-
-public interface UserTransformation {
-
-    User transform(JsonNode payloadNode);
-}
